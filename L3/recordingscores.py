fec3eead47c6b872347dd9fbde72d13a299edc2e
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-07-14 19:46:47 +0100

    Complete exercise 5

--- L3/recordingscores.py
+++ L3/recordingscores.py
@@ -7,14 +7,20 @@ recent_sse=52938
 
 
 #record a score in a DataFrame
-
+previousscores=pd.read_csv('previousscores.csv')
 
 #find the maximum version number
+maxversion=previousscores['version'].max()
 
+thisversion=maxversion+1
 
 #generate rows
-
+new_row_r2 = {'metric':'r2', 'version':thisversion, 'score':recent_r2}
+new_row_sse = {'metric':'sse', 'version':thisversion, 'score':recent_sse}
 
 #write the dataset to a csv
-
+if recent_sse<previousscores.loc[previousscores['metric']=='sse','score'].min():
+    previousscores = previousscores.append(new_row_r2, ignore_index=True)
+    previousscores = previousscores.append(new_row_sse, ignore_index=True)
+    previousscores.to_csv('newscores.csv', index=False)
 
