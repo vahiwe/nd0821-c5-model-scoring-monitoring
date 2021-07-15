8ac9e4bc9d96bbe7811eebba1a505d87f7ec0c53
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-07-15 15:15:55 +0100

    Complete exercise 13

--- L5/appe2.py
+++ L5/appe2.py
@@ -1,22 +1,30 @@
-
 from flask import Flask, request
 import pandas as pd
 
 app = Flask(__name__)
 
+
+def readpandas(filename):
+    thedata=pd.read_csv(filename)
+    return thedata
+
 @app.route('/')
 def index():
     user = request.args.get('user')
-    return "Hello " + user + '\n'
-
-@app.route('/size')
+    return "Hello " + user
 
 
 @app.route('/summary')
+def summary():
+    filename = request.args.get('filename')
+    thedata=readpandas(filename)
+    return str(thedata.mean(axis=1))
+
+@app.route('/size')
+def size():
+    filename = request.args.get('filename')
+    thedata=readpandas(filename)
+    return str(len(thedata.index))
 
 
 app.run(host='0.0.0.0', port=8000)
-
-
-
-
